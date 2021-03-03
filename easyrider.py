73d4e3c612778c25922f08c73a3e4c9a4dd9ec81
Dioni00 <dsanbla@gmail.com>
2021-03-03 18:02:22 +0100

    Stage 2. Added RegExp to check hour and street name

--- easyrider.py
+++ easyrider.py
@@ -1,4 +1,9 @@
 import json
+import re
+
+# Regexp for hours "hh:mm"
+template_hour = r"^([0-2]{1}[0-9]{1}|2[0-3])\:[0-5]{1}[0-9]{1}$"
+template_street = r"^[A-Z]\w+\s?\w+?\s?(Avenue|Street|Road|Boulevard)+$"
  
 counter = [0, 0, 0, 0, 0, 0]
 
@@ -9,19 +14,23 @@ for line in jdata_dict:
         counter[0] += 1
     if type(line['stop_id']) != int or line['stop_id'] == False:
         counter[1] += 1
-    if type(line['stop_name']) != str or line['stop_name'] == "":
+    if type(line['stop_name']) != str or line['stop_name'] == "" or re.match(template_street, line['stop_name']) is None:
         counter[2] += 1
     if type(line['next_stop']) != int:
         counter[3] += 1
     if type(line['stop_type']) != str or line['stop_type'] not in ['O', 'S', 'F', '']:
         counter[4] += 1 
-    if type(line['a_time']) != str or line['a_time'] == "":
+    if type(line['a_time']) != str or line['a_time'] == "" or re.match(template_hour, line['a_time']) is None:
         counter[5] += 1
 
 
-print(f"Type and required field validation: {sum(counter)} errors")
-i = 0
-for errors in jdata_dict[0]:
-    print(f"{errors}: {counter[i]}")
-    i += 1
+print(f"Format validation: {sum(counter)} errors")
+print(f"stop_name: {counter[2]}")
+print(f"stop_type: {counter[4]}")
+print(f"a_time: {counter[5]}")
+#i = 0
+#for errors in jdata_dict[0]:
+#    if counter[i] > 0:
+#        print(f"{errors}: {counter[i]}")
+#    i += 1
         
