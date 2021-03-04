ec4097b14a7637510e0987f9dbcf581776b03d83
Dioni00 <dsanbla@gmail.com>
2021-03-04 13:28:32 +0100

    Stage 3. To improve adding better loop in stops counter

--- easyrider.py
+++ easyrider.py
@@ -6,9 +6,26 @@ template_hour = r"^([0-2]{1}[0-9]{1}|2[0-3])\:[0-5]{1}[0-9]{1}$"
 template_street = r"^[A-Z]\w+\s?\w+?\s?(Avenue|Street|Road|Boulevard)+$"
  
 counter = [0, 0, 0, 0, 0, 0]
+bus_lines = {"128": 0, "256": 0, "512": 0, "1024": 0}
 
+def number_stops(jdata_dict, bus_lines):
+
+    for line in jdata_dict:
+        if line["bus_id"] == 128:
+            bus_lines["128"] += 1
+        if line["bus_id"] == 256:
+            bus_lines["256"] += 1
+        if line["bus_id"] == 512:
+            bus_lines["512"] += 1
+        if line["bus_id"] == 1024:
+            bus_lines["1024"] += 1
+
+    for keys, values in bus_lines.items():
+        print(f"bus_id: {keys}, stops: {values}") 
 # Data imported from input
 jdata_dict = json.loads(input())
+number_stops(jdata_dict, bus_lines)
+'''
 for line in jdata_dict:
     if type(line['bus_id']) != int or line['bus_id'] == False:
         counter[0] += 1
@@ -33,4 +50,4 @@ print(f"a_time: {counter[5]}")
 #    if counter[i] > 0:
 #        print(f"{errors}: {counter[i]}")
 #    i += 1
-        
+'''        
